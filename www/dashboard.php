2295903dbbdcb8ad302a5a628e25dce082502077
Kengo <Kengo@daden-Lenovo>
2017-12-11 14:14:38 +0900

    [change]dashboardにheaderを追加

--- www/dashboard.php
+++ www/dashboard.php
@@ -1,3 +1,8 @@
 <?php require_once("../include/tpl/header.html")?>
-<div class="att-m2x-widget" data-widget-url="https://api-m2x.att.com/v2/widgets/29ff04c6" data-m2x-key="49cbd6da806dba50054a37da96177995"></div>
+<div class="container">
+  <div class="page-header">
+    <h1>Dashboard</h1>
+  </div>
+  <div class="att-m2x-widget" data-widget-url="https://api-m2x.att.com/v2/widgets/29ff04c6" data-m2x-key="49cbd6da806dba50054a37da96177995"></div>
+</div>
 <?php require_once("../include/tpl/footer.html")?>
