a996b6fffbd8e671295647247c189b2240b5b1e9
dadenCKEP <daden.CKEP@gmail.com>
2017-12-12 23:11:08 +0900

    [add]dashbaordにtrello board embedを追加

--- www/dashboard.php
+++ www/dashboard.php
@@ -3,6 +3,10 @@
   <div class="page-header">
     <h1>Dashboard</h1>
   </div>
+  <blockquote class="trello-board-compact">
+    <a href="https://trello.com/b/fWc9H95H/%E7%A0%94%E7%A9%B6">Trello Board</a>
+  </blockquote>
+  <hr>
   <div class="att-m2x-widget" data-widget-url="https://api-m2x.att.com/v2/widgets/29ff04c6" data-m2x-key="49cbd6da806dba50054a37da96177995"></div>
 </div>
 <?php require_once("../include/tpl/footer.html")?>
