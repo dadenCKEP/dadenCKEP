1f9460238d4cc87675dc2651c6a1aa3f6c7acdc0
dadenCKEP <daden.CKEP@gmail.com>
2017-12-28 01:25:38 +0900

    [add]add works.php and AP-MiconCar

--- www/works.php
+++ www/works.php
@@ -3,7 +3,18 @@
   <div class="page-header">
     <h1>Works</h1>
   </div>
-  せいさくちう
+  <div class="thumbnail">
+    <a href="https://wiki.ckep.info/index.php?title=AP-MiconCar"><img src="./image/miconcar.png" alt="AP-MiconCar"></a>
+    <div class="caption">
+      <h3>AP-MiconCar</h3>
+      <p>Arduino Uno用のシールドで、マイコンカーとして使用するためのもの。</p>
+    </div>
+  </div>
+  <h2>その他</h2>
+  <p>上記のテーマと合わせてTrelloの公開ボードにて管理されています。</p>
+  <blockquote class="trello-board-compact">
+    <a href="https://trello.com/b/fWc9H95H/%E7%A0%94%E7%A9%B6">Trello Board</a>
+  </blockquote>
 </div>
 
 <?php require_once("../include/tpl/footer.html")?>
