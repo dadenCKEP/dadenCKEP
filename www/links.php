8c6d359d90a19764ea4a29b64fb5dbb1edcda88b
dadenCKEP <daden.CKEP@gmail.com>
2017-12-27 16:30:31 +0900

    [change]Linkの配置変更

--- www/links.php
+++ www/links.php
@@ -3,13 +3,46 @@
   <div class="page-header">
     <h1>Link</h1>
   </div>
-  <a href="https://twitter.com/dadenCKEP"><i class="fa fa-twitter fa-5x"></i></a>
-  <a href="https://github.com/dadenCKEP"><i class="fa fa-github fa-5x"></i></a>
-  <a href="https://ckep.info/"><i class="fa fa-users fa-5x"></i></a>
-  <a href="https://steamcommunity.com/id/dadenCKEP"><i class="fa fa-steam fa-5x"></i></a>
-  <a href="https://wiki.ckep.info"><i class="fa fa-book fa-5x"></i></a>
-  <a href="http://blog.ckep.info"><i class="fa fa-wordpress fa-5x"></i></a>
-  <a href="https://twitter.com/ranchanCKEP"><i class="fa fa-bell fa-5x"></i></a>
-  <i class="fa fa-amazon fa-5x"></i>(coming soon)
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-3 col-xs-6">
+        <a href="https://twitter.com/dadenCKEP"><i class="fa fa-twitter fa-5x"></i></a>
+      </div>
+      <div class="col-sm-3 col-xs-6">
+        <a href="https://github.com/dadenCKEP"><i class="fa fa-github fa-5x"></i></a>
+      </div>
+      <div class="col-sm-3 col-xs-6">
+        <a href="https://ckep.info/"><i class="fa fa-users fa-5x"></i></a>
+      </div>
+      <div class="col-sm-3 col-xs-6">
+        <a href="https://steamcommunity.com/id/dadenCKEP"><i class="fa fa-steam fa-5x"></i></a>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-3 col-xs-6">
+        <a href="https://wiki.ckep.info"><i class="fa fa-book fa-5x"></i></a>
+      </div>
+      <div class="col-sm-3 col-xs-6">
+        <a href="http://blog.ckep.info"><i class="fa fa-wordpress fa-5x"></i></a>
+      </div>
+      <div class="col-sm-3 col-xs-6">
+        <a href="https://twitter.com/ranchanCKEP"><i class="fa fa-bell fa-5x"></i></a>
+      </div>
+      <div class="col-sm-3 col-xs-6">
+        <a href="https://monappy.jp/u/dadenCKEP">Monappy<br>(Monacoin wallet)
+        </a>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-3 col-xs-6">
+        <i class="fa fa-amazon fa-5x"></i><br>(coming soon)
+      </div>
+      <div class="col-sm-3 col-xs-6"></div>
+      <div class="col-sm-3 col-xs-6"></div>
+      <div class="col-sm-3 col-xs-6"></div>
+    </div>
+  </div>
+
+
 </div>
 <?php require_once("../include/tpl/footer.html")?>
