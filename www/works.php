40984e764c99f4d5297e436ddc8654fbaed7b7c4
dadenCKEP <daden.CKEP@gmail.com>
2018-01-04 03:35:42 +0900

    [add]電脳機巧技師団複合基幹システムを追加
    この名前覚えられないんだけどｗｗｗ

--- www/works.php
+++ www/works.php
@@ -4,10 +4,25 @@
     <h1>Works</h1>
   </div>
   <div class="thumbnail">
-    <a href="https://wiki.ckep.info/index.php?title=AP-MiconCar"><img src="./image/miconcar.png" alt="AP-MiconCar"></a>
+    <img src="./image/miconcar.png" alt="AP-MiconCar">
     <div class="caption">
-      <h3>AP-MiconCar</h3>
+      <h3>
+        <a href="https://wiki.ckep.info/index.php?title=AP-MiconCar">AP-MiconCar</a>
+      </h3>
       <p>Arduino Uno用のシールドで、マイコンカーとして使用するためのもの。</p>
+    </div>
+  </div>
+  <div class="thumbnail">
+    <div class="text-center">
+      <i class="fa fa-server fa-5x"></i>
+    </div>
+    <div class="caption">
+      <h3>
+        <a href="https://wiki.ckep.info/index.php?title=%E9%9B%BB%E8%84%B3%E6%A9%9F%E5%B7%A7%E6%8A%80%E5%B8%AB%E5%9B%A3%E8%A4%87%E5%90%88%E5%9F%BA%E5%B9%B9%E3%82%B7%E3%82%B9%E3%83%86%E3%83%A0">電脳機巧技師団複合基幹システム</a>
+      </h3>
+      <p>
+        このサイトを含む、ckpe.info以下の全てのサイトをホストするシステム。簡単に言えば自宅サーバーとその関係機器。<br>DDNSやリバースプロキシなども含まれる。 <br> <a href="https://wiki.ckep.info/index.php?title=%E9%9B%BB%E8%84%B3%E6%A9%9F%E5%B7%A7%E6%8A%80%E5%B8%AB%E5%9B%A3%E8%A4%87%E5%90%88%E5%9F%BA%E5%B9%B9%E3%82%B7%E3%82%B9%E3%83%86%E3%83%A0">wiki</a>に多少情報があります。
+      </p>
     </div>
   </div>
   <div class="thumbnail">
