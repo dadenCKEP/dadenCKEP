5ed7e36a6bdc9fbed2aafc29a2d72480eb1a9adc
dadenCKEP <dadenCKEP@daden-main>
2018-12-26 03:48:53 +0900

    [add]add works

--- www/works.php
+++ www/works.php
@@ -4,40 +4,54 @@
   <div class="row">
     <div class="col-lg-6 col-sm-12">
       <div class="card m-2">
-        <img class="card-img-top" src="./image/miconcar.png" alt="AP-MiconCar">
+        <img class="card-img-top" src="./image/keyboard.jpg" alt="Helix-CKEP">
         <div class="card-body">
-          <h3 class="card-title">
-            <a href="https://wiki.ckep.info/index.php?title=AP-MiconCar">AP-MiconCar</a>
-          </h3>
-          <p class="card-text">Arduino Uno用のシールドで、マイコンカーとして使用するためのもの。</p>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-6 col-sm-12">
-      <div class="card m-2">
-        <img class="card-img-top" src="./image/server.png" alt="Server">
-        <div class="card-body">
-          <h3>
-            <a href="https://wiki.ckep.info/index.php?title=%E9%9B%BB%E8%84%B3%E6%A9%9F%E5%B7%A7%E6%8A%80%E5%B8%AB%E5%9B%A3%E8%A4%87%E5%90%88%E5%9F%BA%E5%B9%B9%E3%82%B7%E3%82%B9%E3%83%86%E3%83%A0">電脳機巧技師団複合基幹システム</a>
-          </h3>
+          <h3 class="card-title">Helix - CKEP</h3>
           <p class="card-text">
-            このサイトを含む、ckpe.info以下の全てのサイトをホストするシステム。簡単に言えば自宅サーバーとその関係機器。<br>DDNSやリバースプロキシなども含まれる。 <br> <a href="https://wiki.ckep.info/index.php?title=%E9%9B%BB%E8%84%B3%E6%A9%9F%E5%B7%A7%E6%8A%80%E5%B8%AB%E5%9B%A3%E8%A4%87%E5%90%88%E5%9F%BA%E5%B9%B9%E3%82%B7%E3%82%B9%E3%83%86%E3%83%A0">wiki</a>に多少情報があります。
+            オープンソースの分割型格子配置キーボード「<a href="https://github.com/MakotoKurauchi/helix">Helix</a>」をカスタマイズしたもの。
           </p>
         </div>
       </div>
     </div>
-  </div>
-  <div class="row">
     <div class="col-lg-6 col-sm-12">
       <div class="card m-2">
-        <img class="card-img-top" src="https://api.vrchat.cloud/api/1/file/file_de96d768-c0b6-4956-b000-f5d0423839ad/1/file" alt="World">
+        <img class="card-img-top" src="image/vrc.png" alt="VRChat">
         <div class="card-body">
           <h3>CKEP VR Base / daden's hideout</h3>
           <p class="card-text">
-            ゲーム「<a href="https://vrchat.com/">VRChat</a>」のWorldです。WepPanelを中心に、様々な実験をしています。<br> 検証結果等は技術系wikiの<a href="http://vrcworld.wiki.fc2.com/">VRChat 技術メモ帳</a>に参加して書いているためそちらをご参照ください。
+            VRメタバース「<a href="https://vrchat.com/">VRChat</a>」のWorldです。Unity製です。<br>3Dモデル等の製作物もこちらに展示してあります。<br> 各種実験結果等は技術系wikiの<a href="http://vrcworld.wiki.fc2.com/">VRChat 技術メモ帳</a>に参加しています。
           </p>
         </div>
       </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-6 col-sm-12">
+        <div class="card m-2">
+          <img class="card-img-top" src="./image/miconcar.png" alt="AP-MiconCar">
+          <div class="card-body">
+            <h3 class="card-title">
+              <a href="https://wiki.ckep.info/index.php?title=AP-MiconCar">AP-MiconCar</a>
+            </h3>
+            <p class="card-text">Arduino Uno用のシールドで、マイコンカーとして使用するためのもの。</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6 col-sm-12">
+        <div class="card m-2">
+          <img class="card-img-top" src="./image/server.png" alt="Server">
+          <div class="card-body">
+            <h3>
+              <a href="https://wiki.ckep.info/index.php?title=%E9%9B%BB%E8%84%B3%E6%A9%9F%E5%B7%A7%E6%8A%80%E5%B8%AB%E5%9B%A3%E8%A4%87%E5%90%88%E5%9F%BA%E5%B9%B9%E3%82%B7%E3%82%B9%E3%83%86%E3%83%A0">電脳機巧技師団複合基幹システム</a>
+            </h3>
+            <p class="card-text">
+              このサイトを含む、ckpe.info以下の全てのサイトをホストするシステム。簡単に言えば自宅サーバーとその関係機器。<br>DDNSやリバースプロキシなども含まれる。 <br> <a href="https://wiki.ckep.info/index.php?title=%E9%9B%BB%E8%84%B3%E6%A9%9F%E5%B7%A7%E6%8A%80%E5%B8%AB%E5%9B%A3%E8%A4%87%E5%90%88%E5%9F%BA%E5%B9%B9%E3%82%B7%E3%82%B9%E3%83%86%E3%83%A0">wiki</a>に多少情報があります。
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-6 col-sm-12"></div>
     </div>
     <div class="col-lg-6 col-sm-12">
       <div class="card m-2">
@@ -53,7 +67,7 @@
     </div>
   </div>
 
-  <h2 class="mt-3 mb-5 font-weight-bold border-bottom">その他</h2>
+  <h2 class="mt-3 mb-3 font-weight-bold border-bottom">その他(進行中・保留)</h2>
   <p>上記のテーマと合わせてTrelloの公開ボードにて管理されています。</p>
   <blockquote class="trello-board-compact">
     <a href="https://trello.com/b/fWc9H95H/%E7%A0%94%E7%A9%B6">Trello Board</a>
